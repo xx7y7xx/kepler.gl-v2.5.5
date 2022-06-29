84914f54cb9341561143f9bd15dd7fd64503d45b
Devin Chenyang <devin.chenyang@grabtaxi.com>
2022-06-29 18:23:00 +0800

    Add kepler.gl dep and init reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import keplerGlReducer from 'kepler.gl/reducers';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    keplerGl: keplerGlReducer.initialState({
+      uiState: {
+        readOnly: true,
+        mapControls: {
+          visibleLayers: {
+            show: false
+          },
+          mapLegend: {
+            show: false,
+            active: false
+          },
+          toggle3d: {
+            show: false
+          },
+          splitMap: {
+            show: false
+          }
+        }
+      }
+    }),
   },
 });
